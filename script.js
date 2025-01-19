4c4b91f1762a1debada92eefd53448067a2f1ead
Rania984 <raniaboukoummia@gmail.com>
2025-01-19 12:09:11 +0100

    Add files via upload
    Ajout des fichiers du site web

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Merci pour votre message ! Nous vous répondrons sous peu.");
+  });
+  
+  
